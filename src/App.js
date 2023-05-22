925f2e8c37943fbc0e59069779b1cf6d16b9f701
lucascencig <cenciglucas@gmail.com>
2023-05-22 19:28:14 -0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 function App() {
+  const [number, setNumber] = useState('');
+  const [selectedNumbers, setSelectedNumbers] = useState([]);
+  const [operation, setOperation] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClear = () => {
+    setSelectedNumbers([]);
+    setNumber('');
+    setResult('');
+  };
+
+  const handleNumber = (num) => {
+    setNumber((prevNumber) => prevNumber + num);
+    setResult('');
+  };
+
+  const handleOperation = (op) => {
+    setOperation(op);
+    setSelectedNumbers([...selectedNumbers, Number(number)]);
+    setNumber('');
+    setResult('');
+  };
+
+  const calculateResult = () => {
+    const currentNumber = Number(number);
+    let calculatedResult = '';
+
+    switch (operation) {
+      case '/':
+        calculatedResult = selectedNumbers.reduce((acc, num) => acc / num, currentNumber);
+        break;
+      case '*':
+        calculatedResult = selectedNumbers.reduce((acc, num) => acc * num, currentNumber);
+        break;
+      case '+':
+        calculatedResult = selectedNumbers.reduce((acc, num) => acc + num, currentNumber);
+        break;
+      case '-':
+        calculatedResult = selectedNumbers.reduce((acc, num) => acc - num, currentNumber);
+        break;
+      default:
+        break;
+    }
+
+    setResult(calculatedResult.toString());
+    setSelectedNumbers([]);
+    setNumber(calculatedResult.toString());
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='result'>
+          <input type='text' value={number || result} readOnly className='result_view' />
+        </div>
+
+        <div className='operators'>
+          <button onClick={handleClear}>AC</button>
+          <button onClick={() => handleOperation('/')}>÷</button>
+          <button onClick={() => handleOperation('*')}>×</button>
+          <button onClick={() => handleOperation('-')}>–</button>
+          <button onClick={() => handleOperation('+')}>+</button>
+          {/* <button onClick={() => handleNumber(number)}>.</button> */}
+        </div>
+
+        <div className='numbers'>
+          {numbers.map((number) => (
+            <button onClick={() => handleNumber(number)} key={number} className="">
+              {number}
+            </button>
+          ))}
+        </div>
+
+        <div className='cero__point'>
+          <button onClick={() => handleNumber('0')} className='cero'>0</button>
+          <button onClick={() => handleNumber('.')} className='point'>.</button>
+          <button className='equal' onClick={calculateResult}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
